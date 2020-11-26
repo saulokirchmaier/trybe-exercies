adaa72bccc52f136656ff658f8ab218eebd64366
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-26 17:47:09 -0300

    Exercicio 4 - imprimir se a media aritimetica do array eh maior ou menor que 20.

---
+++ bloco_4/dia_2/exercise_4.js
@@ -0,0 +1,19 @@
+// Média aritimética dos elementos do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let totalElements = numbers.length;
+
+for (index = 0; index < totalElements; index += 1) {
+    sum += numbers[index];    
+}; 
+
+let arithmeticMedia = sum / totalElements;
+
+console.log('A soma dos numeros ', numbers, ' é ', sum, ' e possui ', totalElements, ' elementos, assim a média aritimética é ', arithmeticMedia, '.');
+
+if (arithmeticMedia > 20) {
+    console.log('Assim o valor é maior que 20.');
+} else {
+    console.log('Assim o valor é menor que 20.');
+}
