6594af7edbbd1446aeddb7db5878c2ace9d5a01f
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-26 18:27:56 -0300

    Exercicio 7 - utilizando for, imprimir o numero de menor valor dentro do array.

---
+++ bloco_4/dia_2/exercise_7.js
@@ -0,0 +1,12 @@
+// Menor valor dentro do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smallestNumber = numbers[0];
+
+for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] <= smallestNumber) {
+        smallestNumber = numbers[index]
+    }
+}; 
+
+console.log('O menor valor contido no array ', numbers, ' é ', smallestNumber);
