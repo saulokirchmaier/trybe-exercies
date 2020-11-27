ff64e81a01bc158a647feb9237af3d7e8d9e5b54
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-27 18:28:15 -0300

    Exercicio 1 - Adiciona comparacao para o n > 1.

--- bloco_4/dia_3/exercise_1.js
+++ bloco_4/dia_3/exercise_1.js
@@ -1,12 +1,16 @@
 // Imprimi um quadrado * de tamanha n, sendo n informado
 
-let n = 10;
+let n = 5;
 let line = '*';
 
-for (i = 1; i < n; i += 1) {
-    for (j = 1; j < n; j += 1) {
-        line = line + '*';
+if (n > 1) {
+    for (i = 0; i < n; i += 1) {
+        for (j = 1; j < n; j += 1) {
+            line = line + '*';
+        }
+        console.log(line);
+        line = '*';
     }
-    console.log(line);
-    line = '*';
+} else {
+    console.log('Número inválido, favor escolher outro.')
 }
