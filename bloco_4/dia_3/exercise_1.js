8aabda79c5894d264d70be5a755d4d3203ce4ae0
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-27 18:33:07 -0300

    Exercicio 1 - string comeca vazia.

--- bloco_4/dia_3/exercise_1.js
+++ bloco_4/dia_3/exercise_1.js
@@ -1,15 +1,15 @@
 // Imprimi um quadrado * de tamanha n, sendo n informado
 
 let n = 5;
-let line = '*';
+let line = '';
 
 if (n > 1) {
     for (i = 0; i < n; i += 1) {
-        for (j = 1; j < n; j += 1) {
+        for (j = 0; j < n; j += 1) {
             line = line + '*';
         }
         console.log(line);
-        line = '*';
+        line = '';
     }
 } else {
     console.log('Número inválido, favor escolher outro.')
