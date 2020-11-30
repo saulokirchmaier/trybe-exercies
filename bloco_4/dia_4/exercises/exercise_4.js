e3a05e04af6fde21f2b7e1da58c6b3b8af6654b6
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-30 18:08:05 -0300

    Exercicio 4 - Mostra o conteudo das keys do objeto.

---
+++ bloco_4/dia_4/exercises/exercise_4.js
@@ -0,0 +1,13 @@
+// Mostra o conteudo do obejto em um for/in
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+info.recorrente = 'Sim';
+
+for (let key in info) {
+  console.log(info[key]);
+}
