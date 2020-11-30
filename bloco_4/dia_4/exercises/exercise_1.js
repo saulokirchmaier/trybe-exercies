e11948e944220cba445b2390df95c281d02c374a
saulokirchmaier <saulokirchmaier@gmail.com>
2020-11-30 17:58:33 -0300

    Exercicio 1 - Boas vindas a margarida.

---
+++ bloco_4/dia_4/exercises/exercise_1.js
@@ -0,0 +1,9 @@
+// Boas vindas a Margarida
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+console.log('Bem-vinda ' + info.personagem);
