fbccae58941fc7ac8f9a5e52bcc020461a3e9808
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-05 13:48:13 -0300

    Create section for resume

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -119,8 +119,7 @@ function invalidDate(date) {
   alert('A data '+ date + ' é inválida');
 }
 
-function checkDate(event) {
-  event.preventDefault();
+function checkDate() {
   const initialDateJob = document.querySelector('#start-date');
   const dateArray = initialDateJob.value.split('/');
   if (dateArray[0] <= 0 || dateArray[0] > 31) invalidDate(initialDateJob.value);
@@ -128,9 +127,63 @@ function checkDate(event) {
   else if (dateArray[2] < 0) invalidDate(initialDateJob.value);
 }
 
+function createResume(event) {
+  const resume = document.querySelector('.resume');
+  resume.innerHTML = '';
+  event.preventDefault();
+  checkDate();
+  const inputName = document.querySelector('#name');
+  const inputEmail = document.querySelector('#email');
+  const inputCpf = document.querySelector('#cpf');
+  const inputAdress = document.querySelector('#adress');
+  const inputCity = document.querySelector('#city');
+  const inputStates = document.querySelector('#states');
+  const inputHomeType = document.getElementsByName('home-type');
+  const inputResumeText = document.querySelector('#resume-text');
+  const inputRole = document.querySelector('#role');
+  const inputRoleDescription = document.querySelector('#role-description');
+  const inputStartDate = document.querySelector('#start-date');
+
+  const name = document.createElement('p');
+  name.innerText = 'Name: ' + inputName.value;
+  const email = document.createElement('p');
+  email.innerText = 'Email: ' + inputEmail.value;
+  const cpf = document.createElement('p');
+  cpf.innerText = 'CPF: ' + inputCpf.value;
+  const adress = document.createElement('p');
+  adress.innerText = 'Endereço: ' + inputAdress.value;
+  const city = document.createElement('p');
+  city.innerText = 'Cidade: ' + inputCity.value;
+  const state = document.createElement('p');
+  state.innerText = 'Estado: ' + inputStates.value;
+  const homeType = document.createElement('p');
+  if (inputHomeType[0].checked) homeType.innerText = 'Moradia: casa';
+  else homeType.innerText = 'Moradia: apartamento'
+  const resumeText = document.createElement('p');
+  resumeText.innerText = 'Resumo do currículo: ' + inputResumeText.value;
+  const role = document.createElement('p');
+  role.innerText = 'Cargo: ' + inputRole.value;
+  const roleDescription = document.createElement('p');
+  roleDescription.innerText = 'Descrição do cargo: ' + inputRoleDescription.value;
+  const startDate = document.createElement('p');
+  startDate.innerText = 'data de início: ' + inputStartDate.value;
+
+  resume.appendChild(name);
+  resume.appendChild(email);
+  resume.appendChild(cpf);
+  resume.appendChild(adress);
+  resume.appendChild(city);
+  resume.appendChild(state);
+  resume.appendChild(homeType);
+  resume.appendChild(resumeText);
+  resume.appendChild(role);
+  resume.appendChild(roleDescription);
+  resume.appendChild(startDate);
+}
+
 function listener() {
   const buttonSubmit = document.querySelector('#submit');
-  buttonSubmit.addEventListener('click', checkDate);
+  buttonSubmit.addEventListener('click', createResume);
 }
 
 window.onload = function() {
