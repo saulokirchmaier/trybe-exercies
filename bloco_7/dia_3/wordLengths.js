a404941d96e3a328fd9e94f97fd2167d4372b32a
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-15 11:16:43 -0300

    Ex: 7 - TDD 2 - teste unitario comtemplando funcao wordLengths

---
+++ bloco_7/dia_3/wordLengths.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+// escreva a função wordLengths aqui
+
+const wordLengths = (words) => {
+    const wordsLetters = [];
+    for (let index = 0; index < words.length; index += 1) {
+        wordsLetters.push(words[index].length);
+    }
+
+    return wordsLetters;
+};
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
