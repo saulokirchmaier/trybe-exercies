c24e8b57fc9df4b805206dc9d632e20055722db0
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-28 18:54:37 -0300

    Ex. 1 - Shows a joke on DOM.

--- bloco_9_javascript_assincrono_e_promises/day_2/apiScript.js
+++ bloco_9_javascript_assincrono_e_promises/day_2/apiScript.js
@@ -1,7 +1,23 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+
+const appendJoke = (joke) => {
+  const jokeContainer = document.querySelector('#jokeContainer');
+  const jokeElement = document.createElement('p');
+  jokeElement.innerText = joke;
+  jokeContainer.appendChild(jokeElement);
+}
+  
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => appendJoke(data.joke));
 };
+
 
 window.onload = () => fetchJoke();
