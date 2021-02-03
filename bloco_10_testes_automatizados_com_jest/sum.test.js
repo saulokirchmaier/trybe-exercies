2968109649e4de62388fa67fada8ea219b9d5dc7
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-03 18:13:00 -0300

    Ex. 3 -jest test is error if string for param

--- bloco_10_testes_automatizados_com_jest/sum.test.js
+++ bloco_10_testes_automatizados_com_jest/sum.test.js
@@ -1,7 +1,16 @@
 const sum = require('./sum.js');
 
 describe('Testes parte 1 dia 1 bloco 10', () => {
-  it('tests', () => {
-    expect(sum(4,5)).toBe(9);
+  it('Check if 4 + 5 = 9', () => {
+    expect(sum(4, 5)).toBe(9);
   });
+
+  it('Check if 0 + 0 = 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('Check if 4 + "5" return error', () => {
+    expect(() => sum(4, '5')).toThrow();
+  });
+
 });
