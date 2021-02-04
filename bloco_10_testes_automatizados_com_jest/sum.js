4e1157511074d9ce4ee13a56d20c34dddac80bad
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-04 17:59:57 -0300

    folder

--- bloco_10_testes_automatizados_com_jest/sum.js
+++
@@ -1,8 +0,0 @@
-const sum = (num1, num2) => {
-	if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    throw new Error('parameters must be numbers');
-  }
-	return num1 + num2;
-};
-
-module.exports = sum;
--- bloco_10_testes_automatizados_com_jest/techList.js
+++
@@ -1,7 +0,0 @@
-function techList(list, name) {
-    if (!list.length) return 'Vazio!';
-    list = list.sort();
-    return list.map(element => ({tech: element, name}));
-  }
-
-  module.exports = techList;
