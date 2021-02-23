38e7a681a45726398b6ae14b000c468876aa4f56
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-22 23:02:48 -0300

    Ex. 2 select 2 types

--- modulo_2_desenvolvimento_front-end/bloco_12/day_1/my-pokedex/src/Pokedex.js
+++ modulo_2_desenvolvimento_front-end/bloco_12/day_1/my-pokedex/src/Pokedex.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Children } from 'react';
 import './Pokedex.css';
 import Pokemon from './Pokemon';
+// import PokeTypes from './PokeTypes';
 import Button from './Button';
+import PokeTypesButtons from './PokeTypesButtons';
 
 class Pokedex extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      pokemonsToShow: this.props.data,
       currentPokemon: 0,
     }
   }
@@ -19,9 +22,23 @@ class Pokedex extends React.Component {
       this.setState((lastPokemon, _props) => ({currentPokemon: lastPokemon.currentPokemon + 1}));
     }
   };
+
+  handlePokeFilter = (search) => {
+    this.setState((_props) => ({currentPokemon: 0}));
+    const { data } = this.props;
+    if (search === 'All') this.setState((_props) => ({pokemonsToShow: data}));
+    else {
+      const pokeFiltered = data.filter(({type}) => type === search);
+      this.setState((_props) => ({pokemonsToShow: pokeFiltered}));
+    }
+  }
   
   render() {
-    const { data } = this.props;
+    const data = this.state.pokemonsToShow;
+    const pokeTypes = this.props.data
+      .reduce((unic, item) => unic
+        .includes(item.type) ? unic : [...unic, item.type], []);
+    pokeTypes.push('All');
     return (
       <div>
         <h1>Pokedex</h1>
@@ -29,6 +46,12 @@ class Pokedex extends React.Component {
           <Pokemon pokemon={data[this.state.currentPokemon]} key={data.id}/>
         </div>
         <Button handleClick={() => this.handleNextButton(data.length)}>Next</Button>
+        <div>
+          <Button handleClick={() => this.handlePokeFilter('All')}>All</Button>
+          <Button handleClick={() => this.handlePokeFilter('Fire')}>Fire</Button>
+          <Button handleClick={() => this.handlePokeFilter('Psychic')}>Psychic</Button>
+        </div>
+        <PokeTypesButtons pokeTypes={pokeTypes}/>
       </div>
     );
   }
