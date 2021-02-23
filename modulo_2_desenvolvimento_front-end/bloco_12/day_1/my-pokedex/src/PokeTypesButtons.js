2c90aa1bc267fa60079d6dec5d6357f107db64db
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-22 23:45:50 -0300

    botoes dinamicos

---
+++ modulo_2_desenvolvimento_front-end/bloco_12/day_1/my-pokedex/src/PokeTypesButtons.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+
+function PokeTypesButtons(props) {
+  // const {pokeTypes} = this.props;
+  // console.log(pokeTypes);
+  return(
+    <Button>ajhsgahksg</Button>
+  )
+}
+
+export default PokeTypesButtons;
