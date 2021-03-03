9f0938de5adfaa0d4c4933c08d04275009ee0478
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-03 12:16:18 -0300

    Ex. 4/5/6/7

--- modulo_2_desenvolvimento_front-end/bloco_13/day_2/exercise-pokedex-router/src/App.js
+++ modulo_2_desenvolvimento_front-end/bloco_13/day_2/exercise-pokedex-router/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 import Details from './Details';
+import About from './About';
+import NoMatch from './NoMatch';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <BrowserRouter>
-        <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
-        <Route path="/pokemons/:id" render={(props) => <Details {...props} pokemons={pokemons}/>} />
+        <nav>
+          <Link to="/" >Home</Link>
+          <Link to="/about" >About</Link>
+          <Link to="/" >Favorite Pokemon</Link>
+        </nav>
+        <Switch>
+          <Route path="/pokemons/:id" render={(props) => <Details {...props} pokemons={pokemons}/>} />
+          <Route path="/about" component={About} />
+          <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
+          <Route component={NoMatch}/>
+        </Switch>
       </BrowserRouter>
     </div>
   );
