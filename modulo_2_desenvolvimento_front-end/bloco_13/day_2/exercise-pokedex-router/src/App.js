b033552fafd222bd37d4213815f882c09ce627ec
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-03 11:36:19 -0300

    Ex. 2/3 - pokemon detais page

--- modulo_2_desenvolvimento_front-end/bloco_13/day_2/exercise-pokedex-router/src/App.js
+++ modulo_2_desenvolvimento_front-end/bloco_13/day_2/exercise-pokedex-router/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Details from './Details';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1> Pokedex </h1>
       <BrowserRouter>
         <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
+        <Route path="/pokemons/:id" render={(props) => <Details {...props} pokemons={pokemons}/>} />
       </BrowserRouter>
     </div>
   );
