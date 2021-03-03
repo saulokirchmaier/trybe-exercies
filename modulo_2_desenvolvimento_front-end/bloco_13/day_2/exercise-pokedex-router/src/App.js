f79ddbe32111b6ac5e4c68b75cf8f2a829e06992
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-03 10:24:23 -0300

    Ex. 1 - Route home

---
+++ modulo_2_desenvolvimento_front-end/bloco_13/day_2/exercise-pokedex-router/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Pokedex </h1>
+      <BrowserRouter>
+        <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
